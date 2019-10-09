0013246ddbf0b7e6c8d1cf2630a1761dc7717e84
UGASucks <dlim9696@gmail.com>
2019-10-09 17:05:15 -0400

    progress

--- client/src/components/Combat.js
+++ client/src/components/Combat.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Page } from 'react-onsenui';
+import { Page, Card, Icon } from 'react-onsenui';
 
 function Combat() {
+  const [necessaryToolsOpen, changeNecessaryToolsOpen] = React.useState(false);
+  const [optionalToolsOpen, changeOptionalToolsOpen] = React.useState(false);
   return (
     <Page>
-      <h1>Usage of Weapons</h1>
+      <Card>
+      <h1>Overview</h1>
+      </Card>
+      <Card>
+        <p>Overview Text to be done</p>
+      </Card>
+      <Card onClick={() => changeNecessaryToolsOpen(!necessaryToolsOpen)} style={{ display: 'flex', alignItems: 'center' }}>
+      <Icon size={{default: 32}} icon={{default: `md-caret-${necessaryToolsOpen ? 'up' : 'down'}-circle`}} style={{ marginRight: 10 }} /><h1>Necessary Tools</h1>
+      </Card>
+
     </Page>
   )
 }
